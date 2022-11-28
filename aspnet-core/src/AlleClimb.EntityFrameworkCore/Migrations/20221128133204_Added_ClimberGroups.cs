0936e5c4abdd3b9b4821722a3337839bc779fb92
redashimself <114561058+redashimself@users.noreply.github.com>
2022-11-28 15:41:32 +0200

    Add auto generated migrations

---
+++ aspnet-core/src/AlleClimb.EntityFrameworkCore/Migrations/20221128133204_Added_ClimberGroups.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlleClimb.Migrations
+{
+    public partial class Added_ClimberGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppAbp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppAbp", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAbp_Name",
+                table: "AppAbp",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppAbp");
+        }
+    }
+}
---
+++ aspnet-core/src/AlleClimb.EntityFrameworkCore/Migrations/20221128133846_Updated_ClimberGroups.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlleClimb.Migrations
+{
+    public partial class Updated_ClimberGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppAbp",
+                table: "AppAbp");
+
+            migrationBuilder.RenameTable(
+                name: "AppAbp",
+                newName: "AppClimberGroups");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AppAbp_Name",
+                table: "AppClimberGroups",
+                newName: "IX_AppClimberGroups_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppClimberGroups",
+                table: "AppClimberGroups",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppClimberGroups",
+                table: "AppClimberGroups");
+
+            migrationBuilder.RenameTable(
+                name: "AppClimberGroups",
+                newName: "AppAbp");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AppClimberGroups_Name",
+                table: "AppAbp",
+                newName: "IX_AppAbp_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppAbp",
+                table: "AppAbp",
+                column: "Id");
+        }
+    }
+}
