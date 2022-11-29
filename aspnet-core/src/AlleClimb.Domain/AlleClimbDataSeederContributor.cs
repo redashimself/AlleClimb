288aeb96363b7a212ecb4f841831fff1dd81d257
redashimself <114561058+redashimself@users.noreply.github.com>
2022-11-29 17:10:26 +0200

    Add initial climbing groups to database table

---
+++ aspnet-core/src/AlleClimb.Domain/AlleClimbDataSeederContributor.cs
@@ -0,0 +1,32 @@
+﻿namespace AlleClimb;
+
+using System;
+using System.Threading.Tasks;
+using ClimberGroups;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+public class AlleClimbDataSeederContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<ClimberGroup, Guid> _climberGroupRepository;
+
+    public AlleClimbDataSeederContributor(IRepository<ClimberGroup, Guid> climberGroupRepository)
+    {
+        _climberGroupRepository = climberGroupRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _climberGroupRepository.GetCountAsync() <= 0)
+        {
+            await _climberGroupRepository.InsertAsync(
+                new ClimberGroup("Liūtukai", "Liutukų grupė")
+            );
+
+            await _climberGroupRepository.InsertAsync(
+                new ClimberGroup("Kačiukai", "Kačiukų grupė")
+            );
+        }
+    }
+}
